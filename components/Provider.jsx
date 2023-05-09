5299d8fe3d4f2a5e9ac0bd409e3c838ee6ede32b
keroloussamy <keroloussamy98@gmail.com>
2023-05-09 14:17:57 +0300

    Add Google SignIn using next-auth

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,11 @@
-const Provider = () => {
-  return (
-    <div>Provider</div>
-  )
-}
+'use client';
 
-export default Provider
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => (
+  <SessionProvider session={session}>
+    {children}
+  </SessionProvider>
+)
+
+export default Provider;
